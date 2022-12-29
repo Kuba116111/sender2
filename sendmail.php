01d16b3362d0f9734408863f51a9fdd60982328b
unknown <pkuba04@wp.pl>
2022-12-29 20:05:09 +0100

    29.12.2022
    gówno ożyło

--- sendmail.php
+++ sendmail.php
@@ -1,8 +1,17 @@
 <?php
-if (mail("gnito320@gmail.com", "Temat wiadomosci", "Tresc wiadomosci", "From: sender2@vp.pl")) {
+
+$to = "pkuba04@wp.pl";
+$subject = "Temat wiadomości";
+$message = "Treść wiadomości";
+$headers = "From: gnito320@gmail.com" . "\r\n" .
+           "CC: pkuba04@wp.pl";
+
+if (mail($to, $subject, $message, $headers)) {
   echo "Wiadomość została wysłana";
 } else {
   echo "Wystąpił błąd podczas wysyłania wiadomości";
 }
+
+// print_r(phpinfo());
 
 ?>
